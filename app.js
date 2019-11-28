9336b98e56a5e554142fa9d9b09a900d23be33fe
hansajayathilaka <hansajayathilaka@yahoo.com>
2019-11-28 10:51:24 +0530

    Rewrite deleted codes.

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const productRouters = require('./api/routes/prodects');
 const ordersRouters = require('./api/routes/orders');
 
+app.use(morgan(('dev')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Routers Which should Handle request
 app.use('/products', productRouters);
 app.use('/orders', ordersRouters);
+
+// 404 Error handler
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+// Handle All errors
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 module.exports = app;
